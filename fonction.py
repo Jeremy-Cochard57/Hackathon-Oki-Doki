9f6d3f55f1f99b6bbb3ecb550b7acf9f075f2006
JeremyCochard <j.cochard13@gmail.com>
2024-02-23 18:13:12 +0100

    second commit

--- fonction.py
+++ fonction.py
@@ -1,11 +1,25 @@
-from pygeocoder import Geocoder
+"""import geocoder
 
 def verifier_adresse(adresse):
     try:
-        resultat = Geocoder.geocode(adresse)
+        resultat = geocoder.geocode(adresse)
         if resultat.valid_address:
             return True, "Adresse valide."
         else:
             return False, "Adresse invalide. Veuillez vérifier et réessayer."
     except Exception as e:
         return False, str(e)
+        """
+
+from geopy.geocoders import Nominatim
+
+def verifier_adresse(adresse):
+    geolocator = Nominatim(user_agent="app hackathon") #instance
+    location = geolocator.geocode(adresse)
+
+    ##return location
+    if location:
+        return True, "Adresse valide !\nVeuillez vous rendre à cette adresse : " + str(location)
+    else:
+        return False, "Adresse invalide. Veuillez vérifier et réessayer."
+        
--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+import os
+
 from fonction import verifier_adresse
 
-adresse = input("Veuillez entrer l'adresse à vérifier : ")
-print(adresse)
+stop = True
+while stop:
+    os.system('cls')
+    numéro = input("Veuillez entrer le numéro de la rue : ")
+    rue = input("Veuillez entrer la rue (sans les caractères spéciales) : ")
+    ville = input("Veuillez entrer la ville (sans les caractères spéciales) : ")
+    adresse = numéro + rue + ville
+    valide, message = verifier_adresse(adresse)
+    print(message)
+
+    if adresse == "stop":
+        stop = False
