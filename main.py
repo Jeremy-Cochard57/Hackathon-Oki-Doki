976e4878cedeea7f6358d200890c1e167bcff60b
Jérémy <JeremyCochard>
2024-02-23 18:25:59 +0100

    correction second commit

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from fonction import verifier_adresse
 
 stop = True
 while stop:
-    os.system('cls')
+    #os.system('cls')
     numéro = input("Veuillez entrer le numéro de la rue : ")
-    rue = input("Veuillez entrer la rue (sans les caractères spéciales) : ")
-    ville = input("Veuillez entrer la ville (sans les caractères spéciales) : ")
-    adresse = numéro + rue + ville
+    rue = input("Veuillez entrer la rue : ")
+    ville = input("Veuillez entrer la ville : ")
+    adresse = numéro + " " + rue + " " + ville
     valide, message = verifier_adresse(adresse)
     print(message)
+    print(adresse)
+    
 
     if adresse == "stop":
         stop = False
