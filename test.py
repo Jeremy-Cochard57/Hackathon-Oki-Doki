71a36cd7a2fa6983d11167832cfd0d6132e83068
Jérémy <JeremyCochard>
2024-02-24 11:39:16 +0100

    pour néo

--- test.py
+++ test.py
@@ -6,21 +6,29 @@ async def recup_coordonnees(address):#Fonction asynchroniser pour faire des requ
     async with ClientSession() as session:
         async with session.get(f"https://nominatim.openstreetmap.org/search?q={address}&format=json") as resultatJSON:
             data = await resultatJSON.json()#Attendre le traitement du fichier JSON
-            if data:
-                for i in data:
-                    localisation=[]
-                    recup_lon_lat=[]
+            """if data:
+                localisation=[]
+                recup_lon=[]
+                recup_lat=[]
+                recup_lon_lat=[]
+                
+               for i in data:
+                   
+                    recup_lon = i.get('lon')
+                    recup_lat = i.get('lat')
+                    recup_lon_lat=recup_lon+""+recup_lat
 
-                    recup_lon_lat = i.get('lon'), i.get('lat')
                     localisation.append(recup_lon_lat)
 
                 return localisation
             else:
-                return None
+                return None"""
+            return data
 
 async def main(adresse):#Coroutine principale de asyncio
     localisation_lon_lat = await recup_coordonnees(adresse)
-    return localisation_lon_lat
+    return localisation_lon_lat 
+            
         
 
 # Exécute la boucle principale asyncio
